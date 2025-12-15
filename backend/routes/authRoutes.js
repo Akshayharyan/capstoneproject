08ba9f9c29c1b907627bc2279b79d41010109cb5
Akshay <akshayharyan11@gmail.com>
2025-12-15 19:41:10 +0530

    bug fixed

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const { registerUser, loginUser } = require("../controllers/authController");
 
-const { registerUser, loginUser } = require("../controllers/authController"); // ✔ correct import
-
-// REGISTER
 router.post("/register", registerUser);
-
-// LOGIN
 router.post("/login", loginUser);
 
 module.exports = router;
